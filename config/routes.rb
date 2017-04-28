ec6ea61595e2935971c7c7c20dc1f7188d86cb05
Sara Gallagher <sara.p.gallagher@gmail.com>
2017-04-27 17:01:01 -0700

    updated follower/following modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'recipes#index'
 
   get 'relationships/follow_user'
 
   get 'relationships/unfollow_user'
-
 
   resources :recipes do
     resources :comments
@@ -11,17 +11,15 @@ Rails.application.routes.draw do
 
   get '/about'=> 'about#index', as: :about
 
-  delete '/logout' => 'sessions#destroy', as: :logout
   resources :sessions, only: [:new, :create]
 
-  root 'recipes#index'
   resources :users
-  get '/users/:id/following' => 'users#following', as: :following
-  get '/users/:id/follower' => 'users#follower', as: :follower
-
 
   post ':id/follow_user', to: 'relationships#follow_user', as: :follow_user
   post ':id/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
+
+  delete '/logout' => 'sessions#destroy', as: :logout
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
